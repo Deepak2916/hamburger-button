9dcbb8f921271b4f4ef27c1a585a35c1f76aa28f
deepak2916 <deepakelimilla@gmail.com>
2022-11-16 18:16:56 +0530

    image-zoomer

---
+++ Image Zoomer/app.js
@@ -0,0 +1,14 @@
+const container=document.getElementById("container")
+
+const img=document.querySelector('img')
+
+container.addEventListener('mouseover',(e)=>{
+    const x=e.clientX-e.target.offsetLeft;
+    const y=e.clientY-e.target.offsetTop;
+  
+
+    img.style.transformOrigin=`${x}px ${y}px`
+    img.style.transform="scale(2)"
+
+
+})
